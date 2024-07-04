1b45652917d0767bb939b83f68afffab2ea1a481
OwerZeroTwo <161704626+OwerZeroTwo@users.noreply.github.com>
2024-07-04 18:17:27 +0300

    Add files via upload

---
+++ Date and time.py
@@ -0,0 +1,31 @@
+from datetime import datetime
+
+class SuperDate(datetime):
+    def __new__(cls, year, month, day, hour, minute=0, second=0):
+        return datetime.__new__(cls, year, month, day, hour, minute, second)
+
+    def get_season(self):
+        month = self.month
+        if month in [12, 1, 2]:
+            return "Winter"
+        elif month in [3, 4, 5]:
+            return "Spring"
+        elif month in [6, 7, 8]:
+            return "Summer"
+        else:
+            return "Autumn"
+
+    def get_time_of_day(self):
+        hour = self.hour
+        if 6 <= hour < 12:
+            return "Morning"
+        elif 12 <= hour < 18:
+            return "Day"
+        elif 18 <= hour < 24:
+            return "Evening"
+        else:
+            return "Night"
+
+a = SuperDate(2024, 2, 22, 12)
+print(a.get_season())
+print(a.get_time_of_day())
